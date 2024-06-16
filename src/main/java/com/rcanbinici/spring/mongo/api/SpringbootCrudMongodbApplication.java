68a464d4e9b0b69580e61c86944cbdb115f40acb
rcanbinici <canbinici.55@gmail.com>
2024-06-16 18:52:13 +0300

    first commit

---
+++ src/main/java/com/rcanbinici/spring/mongo/api/SpringbootCrudMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.rcanbinici.spring.mongo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcanbinici/spring/mongo/api/SpringbootCrudMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcanbinici.spring.mongo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
